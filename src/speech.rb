69ab7b4ad0eaeecdeebcf2bc3cafe6071a90fb78
Alex Lapchenko <ozeron@me.com>
2014-07-09 23:16:18 -0400

    UPDATED Speech class\nIn initialize second parametr now optional\nAdded to_s method

--- src/speech.rb
+++ src/speech.rb
@@ -1,12 +1,15 @@
 module Casting
 	class Speech
 		attr_accessor :title, :text
-		def initialize( title,  text)
+		def initialize( title,  text = nil)
 			@title = ( title.nil? ?  "" : title )
 			@text = ( text.nil? ? "" : text )
 		end
 		def length
 			@title.length + @text.length
+		end
+		def to_s
+			"#{@title}\n#{@text}"
 		end
 	end
 end
